1cb01f54c93d1bbbc53f5d9db4cc66ca3852529e
Stefan Kerkewitz <stefan.kerkewitz@gmail.com>
2017-05-21 13:06:26 +0200

    Add missing class.

---
+++ src/jake2/client/DisplayMode.java
@@ -0,0 +1,25 @@
+package jake2.client;
+
+/**
+ * Created by tropper on 19.05.17.
+ */
+public class DisplayMode {
+    private int width;
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
